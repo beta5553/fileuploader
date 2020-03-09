08851f3e323fc080fe5f276785bb249b64e0a5dc
Izzy <beta5553@gmail.com>
2020-03-09 14:13:29 -0600

    navigation menu

---
+++ src/NavigationMenu/NavigationMenu.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class NavigationMenu extends Component {
+
+    render() {
+        return(
+            <div>
+                Left Navigation Menu`
+            </div>
+        )
+    }
+
+}
+
+export default NavigationMenu
