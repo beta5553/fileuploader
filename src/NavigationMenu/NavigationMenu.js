bd389dfc703b8da33eb175aaadd2577cffea916b
Izzy <beta5553@gmail.com>
2020-03-12 10:30:13 -0600

    more menu options

--- src/NavigationMenu/NavigationMenu.js
+++ src/NavigationMenu/NavigationMenu.js
@@ -15,6 +15,15 @@ class NavigationMenu extends Component {
                     <li>Elemt6</li>
                     <li>Elemt7</li>
                 </ol>
+                <ol> 
+                    <li>Elemt1</li>
+                    <li>Elemt2</li>
+                    <li>Elemt3</li>
+                    <li>Elemt4</li>
+                    <li>Elemt5</li>
+                    <li>Elemt6</li>
+                    <li>Elemt7</li>
+                </ol>
             </div>
         )
     }
